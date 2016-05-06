f60282df17df838d64a5bd6ac601d32fd87ee3c4
Benedikt Reiser <benedikt.reiser@innoactive.de>
2016-05-06 15:27:18 +0200

    editorconfig, linting and package.json (author, etc.)

---
+++ test/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "extends": "defaults/configurations/eslint",
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+          "sourceType": "script",
+          "modules": false
+        },
+    },
+    "env": {
+      "es6": true,
+      "node": true,
+      "mocha": true,
+    },
+    "rules": {
+      "no-restricted-globals": 0,
+      "strict": [2, "global"]
+    }
+};
