7bfd84105fc2b0b6850b7abf477b338a261cde77
Benedikt Reiser <benedikt.reiser@innoactive.de>
2016-05-06 17:09:52 +0200

    added support for creating tasks (as admin only)

--- index.js
+++ index.js
@@ -2,13 +2,131 @@
 const execa = require('execa');
 const parse = require('csv-parse');
 
+const ScheduleTypeEnum = Object.freeze({
+   MINUTE: "MINUTE",
+   HOURLY: "HOURLY",
+   DAILY: "DAILY",
+   WEEKLY: "WEEKLY",
+   MONTHLY: "MONTHLY",
+   ONCE: "ONCE",
+   ONSTART: "ONSTART",
+   ONLOGON: "ONLOGON",
+   ONIDLE: "ONIDLE" 
+});
+
 const schtasks = (operation, args) => {
     let _args = [`/${operation}`].concat(args);
     return execa('schtasks', _args);
 }
 
 module.exports.create = (opts) => {
-    return schtasks('create', opts);
+    let _opts = Object.assign({
+        taskName: 'My Task Name',
+        scheduleType: ScheduleTypeEnum.ONIDLE,
+        taskRun: '',
+        force: true
+    }, opts);
+    let args = [];
+    // support for different arguments, check https://technet.microsoft.com/en-us/library/cc772785(v=ws.10).aspx#BKMK_query
+    for (let opt in _opts) {
+        let optVal = _opts[opt];
+        switch (opt) {
+            case 'taskName':
+                args.push('/tn');
+                args.push(`${optVal}`);
+                break;
+            case 'scheduleType':
+                args.push('/sc');
+                args.push(optVal);
+                break;
+            case 'taskRun':
+                args.push('/tr');
+                args.push(`${optVal}`);
+                break;
+            case 'computer':
+                args.push('/s');
+                args.push(optVal);
+                break;
+            case 'user':
+                args.push('/u');
+                args.push(optVal);
+                break;
+            case 'password':
+                args.push('/s');
+                args.push(optVal);
+                break;
+            case 'privilegedUser':
+                args.push('/ru');
+                args.push(optVal);
+                break;
+            case 'privilegedPassword':
+                args.push('/rp');
+                args.push(optVal);
+                break;
+            case 'modifier':
+                args.push('/mo');
+                args.push(optVal);
+                break;
+            case 'day':
+                args.push('/d');
+                args.push(optVal);
+                break;
+            case 'month':
+                args.push('/m');
+                args.push(optVal);
+                break;
+            case 'idleTime':
+                args.push('/i');
+                args.push(optVal);
+                break;
+            case 'startTime':
+                args.push('/st');
+                args.push(optVal);
+                break;
+            case 'interval':
+                args.push('/ri');
+                args.push(optVal);
+                break;
+            case 'endTime':
+                args.push('/et');
+                args.push(optVal);
+                break;
+            case 'duration':
+                args.push('/du');
+                args.push(optVal);
+                break;
+            case 'stop':
+                args.push('/k');
+                break;
+            case 'startDate':
+                args.push('/sd');
+                args.push(optVal);
+                break;
+            case 'endDate':
+                args.push('/ed');
+                args.push(optVal);
+                break;
+            case 'onlyRunAs':
+                args.push('/it');
+                break;
+            case 'deleteTaskOnScheduleCompletion':
+                args.push('/Z');
+                break;
+            case 'force':
+                args.push('/F');
+                break;
+        }
+    }
+    return schtasks('create', args).then((result) => {
+        return result;
+    }, (err) => {
+        if (err.stderr.indexOf('ERROR: Access is denied.') > -1) {
+            console.error('Please run schtasks in a bash / cmd with admin rights. Else, creating scheduled tasks is not allowed')
+        } else {
+            console.log(err);
+        }
+        throw err;
+    });
 }
 
 module.exports.query = (opts) => {
--- test/test.js
+++ test/test.js
@@ -6,10 +6,33 @@ const schtasks = require('../');
 
 describe('Taskscheduler', function() {
     it('should list all scheduled tasks', function(done) {
-      schtasks.query().then((tasks) => {
-          expect(tasks).to.be.an('array');
-          expect(tasks).to.have.length.at.least(1);
-          done();
-      }, done);
+        schtasks.query().then((tasks) => {
+            expect(tasks).to.be.an('array');
+            expect(tasks).to.have.length.at.least(1);
+            done();
+        }, done);
+    });
+    
+    it('should create a scheduled task that runs on logon and delete it again', function(done) {
+        const taskData = {
+            taskName: 'My Task Name',
+            taskRun: 'notepad.exe',
+            scheduleType: 'ONLOGON',
+            privilegedUser: 'SYSTEM'
+        };
+        schtasks.create(taskData).then(() => {
+            schtasks.query().then((tasks) => {
+                expect(tasks).to.be.an('array');
+                expect(tasks).to.have.length.at.least(1);
+                // make sure the new task is in the list of tasks
+                let needle = tasks.find((task) => {
+                    return task.taskName === `\\${taskData.taskName}`;
+                });
+                expect(needle).to.not.be.equal(undefined);
+                expect(needle).to.not.be.equal(null);
+                // be nice and remove it again
+                done();
+            }, done);
+        }, done);
     });
 });
